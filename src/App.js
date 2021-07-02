9c27861d139d4e639c8ad44df047952ebc9c0f90
GunwooKim <gunwoo@frientrip.com>
2021-07-02 23:50:42 +0900

    node_module 제거

--- src/App.js
+++ src/App.js
@@ -4,13 +4,10 @@ import Login from './Login/LoginPage';
 import SignIn from './Login/SignIn/SignIn';
 import SignUp from './Login/SignUp/SignUp';
 import InputForm from './Login/SignUp/InputForm';
-
 import './css/screen.css';
-import GlobalStyle from './global-style';
 function App() {
   return (
     <div className="screen">
-      <GlobalStyle />
       <Route path="/" component={Login} exact />
       <Route path="/SignIn" component={SignIn} />
       <Route path="/SignUp" component={SignUp} exact />
