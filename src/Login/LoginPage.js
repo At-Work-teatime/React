15160c8b56f9b91cedaf1731773de64478a998fa
anagyeong <dhsdb603519@gmail.com>
2021-07-16 20:44:59 +0900

    Add css file
    로그인 페이지만 css 적용했습니다!

--- src/Login/LoginPage.js
+++ src/Login/LoginPage.js
@@ -1,20 +1,20 @@
 import React from 'react';
-import { Button } from 'antd';
 import { Link } from 'react-router-dom';
-/* 이 부분 뭘 import하는 걸 의미하는지 모르겠어요! 버튼에 링크가 걸려서 사용하는 건가?!*/
-import '../css/pageBase.css';
 
-function Login() {
+function Login({history}) {
   return (
-    <div className="pageBase">
-      <h1>로그인/회원가입 페이지</h1>
-      <Link to="./SignUp">
-        <Button size={'large'}>AtWork가 처음이신가요?</Button>
-      </Link>
-      <br></br>
-      <Link to="./SignIn">
-        <Button size={'large'}>이미 계정이 있나요?</Button>
-      </Link>
+    <div>
+      <section class="title__main">
+      직장생활을 위한 네트워킹 플랫폼, at-work!
+      </section>
+      <form class="button-form-full">
+          <button class="button-full" onClick={() => history.push('./SignUp')}>
+          계정 만들기
+          </button>
+          <button class="button-full" onClick={() => history.push('./SignIn')}>
+          이미 계정이 있어요!
+          </button>
+      </form>
     </div>
   );
 }
